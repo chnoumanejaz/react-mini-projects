99267339d876022b26dcf9849799dea8096cb316
M. Nouman Ejaz <noumanejaz92@gmail.com>
2023-08-16 14:57:07 +0500

    pizza menu complete (only design)

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -1,8 +1,146 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const pizzaData = [
+  {
+    name: 'Focaccia',
+    ingredients: 'Bread with italian olive oil and rosemary',
+    price: 6,
+    photoName: 'pizzas/focaccia.jpg',
+    soldOut: false,
+  },
+  {
+    name: 'Pizza Margherita',
+    ingredients: 'Tomato and mozarella',
+    price: 10,
+    photoName: 'pizzas/margherita.jpg',
+    soldOut: false,
+  },
+  {
+    name: 'Pizza Spinaci',
+    ingredients: 'Tomato, mozarella, spinach, and ricotta cheese',
+    price: 12,
+    photoName: 'pizzas/spinaci.jpg',
+    soldOut: false,
+  },
+  {
+    name: 'Pizza Funghi',
+    ingredients: 'Tomato, mozarella, mushrooms, and onion',
+    price: 12,
+    photoName: 'pizzas/funghi.jpg',
+    soldOut: false,
+  },
+  {
+    name: 'Pizza Salamino',
+    ingredients: 'Tomato, mozarella, and pepperoni',
+    price: 15,
+    photoName: 'pizzas/salamino.jpg',
+    soldOut: true,
+  },
+  {
+    name: 'Pizza Prosciutto',
+    ingredients: 'Tomato, mozarella, ham, aragula, and burrata cheese',
+    price: 18,
+    photoName: 'pizzas/prosciutto.jpg',
+    soldOut: false,
+  },
+];
+
+function App() {
+  return (
+    <div className="container">
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Pizza Shop Co.</h1>
+    </header>
+  );
+}
+
+function Menu() {
+  return (
+    <main className="menu">
+      <h2>Our Menu</h2>
+      {pizzaData.length > 0 ? (
+        <React.Fragment>
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from our stone oven, all organic, all delicious.
+          </p>
+          <ul className="pizzas">
+            {pizzaData.map(pizza => (
+              <PizzaCard pizza={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </React.Fragment>
+      ) : (
+        <p>We're working on our menu please come back later</p>
+      )}
+    </main>
+  );
+}
+
+function Footer() {
+  const currentHour = new Date().getHours();
+  const openHour = 11;
+  const closeHour = 22;
+  const isOpen = currentHour >= openHour && currentHour < closeHour;
+
+  return (
+    <footer className="footer">
+      {isOpen ? (
+        <OpenHour openHour={openHour} closeHour={closeHour} />
+      ) : (
+        <CloseHour openHour={openHour} closeHour={closeHour} />
+      )}
+    </footer>
+  );
+}
+
+function PizzaCard({ pizza }) {
+  // if (pizza.soldOut) return null;
+  return (
+    <li className={`pizza ${pizza.soldOut ? 'sold-out' : ''}`}>
+      <img src={pizza.photoName} alt={pizza.name} />
+      <div>
+        <h3>{pizza.name}</h3>
+        <p>{pizza.ingredients}</p>
+        <span>
+          {pizza.soldOut ? 'SOLD OUT' : 'Price: ' + pizza.price + '$'}{' '}
+        </span>
+      </div>
+    </li>
+  );
+}
+
+function OpenHour({ openHour, closeHour }) {
+  return (
+    <div className="order">
+      <p>
+        we're open from {openHour}:00 to {closeHour}:00. Come visit us or order
+        online.
+      </p>
+      <button className="btn">Order Now</button>
+    </div>
+  );
+}
+
+function CloseHour({ openHour, closeHour }) {
+  return (
+    <div className="order">
+      <p>
+        Sorry, we're open between {openHour}:00 to {closeHour}:00.
+      </p>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,8 +148,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
